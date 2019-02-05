dd82f3716790438fe3e21e4476367de50083b287
Madelyn Y <maddy13vb@gmail.com>
2019-02-05 09:51:20 -0500

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-
+//need to be inside script tag???
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
@@ -7,11 +7,11 @@ var guessesLeft = [];
 
 //math.floor
 //on.key
-//user guess//
+
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
-    guessesMade.push(userGuess);
+
     var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
     if (guessesMade === computerGuess) {
@@ -25,4 +25,8 @@ document.onkeyup = function (event) {
     if (guessesLeft === 0) {
         losses++;
     };
-};
+    userGuess.textContent = "You guessed: " + userGuess;
+}; //not showing up???
+
+// Letters showing up in console.log but not browser???
+
