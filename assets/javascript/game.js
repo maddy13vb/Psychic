0c0241e8544b2deb345e7e9b958f193c8cd7fb2b
Madelyn Y <maddy13vb@gmail.com>
2019-02-06 19:53:01 -0500

    finally working!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,37 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesMade = 5;
-var guessesLeft = [];
+var guessesMade = [];
+var guessesLeft = 5;
 
 //math.floor
 //on.key
-
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
     var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
-    if (guessesMade === computerGuess) {
+    if (userGuess === computerGuess) { //if userGuess is same as computerGuess, guessesLeft decreases and wins increases.
         guessesLeft--;
         wins++;
     };
-    if (guessesMade !== computerGuess) {
+    if (userGuess !== computerGuess) { //if userG is not same as computer, guessesLeft decreases, guesses made displays the letters.
         guessesLeft--;
-        letters.push(guessesMade);
+        guessesMade.push(userGuess);  //userguess is pushed into guessesMade (Don't just read left to right)//
     };
-    if (guessesLeft === 0) {
-        losses++;
-    };
-    userGuess.textContent = "You guessed: " + userGuess;
-}; //not showing up???
+    if (guessesLeft < 0) { //if guessesLeft is less than 0, losses increase.
+        losses++;           // max guesses is 5 before game starts over. 
+        guessesLeft = 5
+        document.getElementById("guesses-left").innerHTML = "Guesses Left:" + guessesLeft;
+        guessesMade = []
+        document.getElementById("guesses-made").innerHTML = "Your guesses so far:" + guessesMade;
+    }
 
-// Letters showing up in console.log but not browser???
+    document.getElementById("wins").innerHTML = "Wins:" + wins; //in order for html to display, this needs to be present. An Id needs to be attached to each p in this case.
+    document.getElementById("losses").innerHTML = "Losses:" + losses;
+    document.getElementById("guesses-left").innerHTML = "Guesses Left:" + guessesLeft;
+    document.getElementById("guesses-made").innerHTML = "Your guesses so far:" + guessesMade;
+};
+
 
